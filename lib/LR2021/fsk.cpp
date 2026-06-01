7b6c973d0807355cf857898ad13128f6a7702cae
Eddie <eddietang2314@gmail.com>
2026-05-31 19:40:43 -0700

    let's simplify the code ig

--- lib/LR2021/fsk.cpp
+++ lib/LR2021/fsk.cpp
@@ -99,44 +99,28 @@ LR2021Error LR2021FSKDriver::transmit(uint8_t *data, uint8_t len)
 LR2021Error LR2021FSKDriver::transmitBurst(uint8_t **packets, int count, uint8_t len)
 {
 
-    // RadioTxFifo write opcode: 0x00 0x02
     static uint8_t cmd[PAYLOAD_SIZE_FSK + 2] = {0x00, 0x02};
-    memcpy(&cmd[2], packets[0], len);
-    spiWrite(cmd, len + 2);
-
     uint8_t setTxCmd[] = {0x02, 0x0D, 0x00, 0x00, 0x00, 0x00};
-    spiWrite(setTxCmd, sizeof(setTxCmd));
+    unsigned long start = micros();
 
-    for (int i = 1; i < count; i++)
+    for (int i = 0; i < count; i++)
     {
-        radioEvent = false;
-        while (!radioEvent)
-            taskYIELD(); // will change speeds?
-
-        // there has been a radio event, lets load the next one:
-        radioEvent = false;
-        uint32_t irq = readIRQ();
-        // not TxDone
-        if (!(irq & (1UL << 19)))
-            return LR2021Error{LR2021_ERR_TX_TIMEOUT, 0};
-
         memcpy(&cmd[2], packets[i], len);
         spiWrite(cmd, len + 2);
         spiWrite(setTxCmd, sizeof(setTxCmd));
-    }
 
-    // final packet's TxDone beacuse we never got to it.
-    radioEvent = false;
-    unsigned long start = micros();
-    while (!radioEvent)
-    {
-        if (micros() - start > 3000 * 1000)
+        radioEvent = false;
+        while (!radioEvent)
+        {
+            if (micros() - start > 3000 * 1000)
+                return LR2021Error{LR2021_ERR_TX_TIMEOUT, 0};
+        }
+
+        radioEvent = false;
+        uint32_t irq = readIRQ();
+        if (!(irq & (1UL << 19)))
             return LR2021Error{LR2021_ERR_TX_TIMEOUT, 0};
     }
-    radioEvent = false;
-    uint32_t irq = readIRQ();
-    if (!(irq & (1UL << 19)))
-        return LR2021Error{LR2021_ERR_TX_TIMEOUT, 0};
 
     return LR2021Error{LR2021_ERR_NONE, 0};
 }
